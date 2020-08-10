b1e7cb9f0830f0404ee9d0ff0d4cb00b7b2f4a12
Drin <60983893+Snakescapes@users.noreply.github.com>
2020-08-10 12:58:29 +0300

    Add files via upload
    Still not finished, but final work version at the moment.

--- coursera_sql_task.py
+++ coursera_sql_task.py
@@ -1,4 +1,5 @@
-import pyodbc
+# import pyodbc
+import csv, sqlite3
 from datetime import datetime
 
 student_pins_to_check = input("Which students would you like to check? Please list PINs (comma separated): ").split(', ')
@@ -8,15 +9,21 @@ end_date = datetime.strptime(input('Please enter end date (format Year-Month-Day
 output_format = input('Please choose output format (html/csv). If none chosen, both will be generated.')
 directory_path = input('Please select output directory path: ')
 
-'''
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-SS7I0B0\SQLEXPRESS;'
+                      'Database=coursera;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('''
 SQL query blueprint:
 
 DECLARE @Pin VARCHAR(10)
-SET @Pin = student_pins_to_check
+SET @Pin = {0}
 DECLARE @StartDate date
-SET @StartDate = CONVERT(datetime, CONVERT(VARCHAR(8), start_date))
+SET @StartDate = CONVERT(datetime, CONVERT(VARCHAR(8), {1}))
 DECLARE @EndDate date
-SET @EndDate = CONVERT(datetime, CONVERT(VARCHAR(8), end_date))
+SET @EndDate = CONVERT(datetime, CONVERT(VARCHAR(8), {2}))
 
 SELECT s.first_name + ' ' + s.last_name as 'Student',
  c.[name] as 'Course Name', c.total_time as 'Total  time', c.credit as 'Credit',
@@ -31,4 +38,9 @@ JOIN instructors as i
 ON c.instructor_id = i.id
 WHERE student_pin = @Pin AND sc.completion_date >= @StartDate AND sc.completion_date <= @EndDate
 GROUP BY s.first_name, s.last_name, c.[name], c.total_time, c.credit, i.first_name, i.last_name
-'''
+'''.format(student_pins_to_check, start_date, end_date))
+
+
+if output_format == "csv" or '':
+    with open('student_report.csv', mode='w') as report:
+        report_writer = csv.writer(report, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
